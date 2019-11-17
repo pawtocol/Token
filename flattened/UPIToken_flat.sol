605fc3a5aa2425576d256a39b5296a3291078d30
Rob Hitchens <26111999+rob-Hitchens@users.noreply.github.com>
2019-11-17 02:15:11 -0700

    minor

--- flattened/UPIToken_flat.sol
+++ flattened/UPIToken_flat.sol
@@ -681,7 +681,7 @@ contract ERC20Burnable is Context, ERC20 {
 
 contract UPIToken is ERC20, ERC20Detailed, ERC20Mintable, ERC20Burnable {
   
-    string private _name = "PAWTOCOL Network UPI Token";
+    string private _name = "Pawtocol Network UPI Token";
     string private _symbol = "UPI";
     uint8 private _decimals = 18;
     uint256 private INITIAL_SUPPLY = uint(1000000000) * (uint256(10) ** _decimals);
