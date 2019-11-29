d1871fbb58b5162fdb75ddc43ab0633fb86173cc
Rob Hitchens <rob.hitchens.mobile@gmail.com>
2019-11-29 12:41:45 -0700

    clean up, minor

--- contracts/UPIToken.sol
+++ contracts/UPIToken.sol
@@ -1,8 +1,6 @@
 pragma solidity 0.5.11;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
 
 contract UPIToken is ERC20Detailed,  ERC20Burnable {
